5f0137d7acc114dfc57e8c45f8ee1c47cf85476d
divamkant16 <kantdivam@gmail.com>
2025-07-17 11:35:41 +0530

    Remove mongoose import

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ListingItem from '../components/ListingItem';
-import { set } from 'mongoose';
 
 export default function Search() {
     const [sidebarData, setSidebarData] = useState({
